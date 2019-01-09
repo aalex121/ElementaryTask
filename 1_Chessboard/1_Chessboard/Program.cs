4c16cf0615dd466c227a4a5efad92f27b4b5f004
aalex121 <zhtlasjur@gmail.com>
2019-01-09 21:31:22 +0200

    Initial commit.

---
+++ 1_Chessboard/1_Chessboard/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Chessboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (UI.UserInputIsValid(args))
+            {
+                int width = Int32.Parse(args[0]);
+                int height = Int32.Parse(args[1]);
+
+                Chessboard board1 = new Chessboard(width, height);
+                UI.DrawChessboard(board1);
+            }
+        }
+    }
+}
