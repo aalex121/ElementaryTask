b52e61e5fb1f0336470bdf4d326c0f62ce9ef196
aalex121 <zhtlasjur@gmail.com>
2019-01-10 14:00:38 +0200

    Initial commit.

---
+++ 3_TriangleSort/3_TriangleSort/Application.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_TriangleSort
+{
+    static class Application
+    {
+        public static bool Run()
+        {
+            SortedSet<Triangle> trSet = new SortedSet<Triangle>(new TriangleComparer());
+
+            string name;
+            double side1;
+            double side2;
+            double side3;
+
+            do
+            {
+                UI.ReqestTriangleParams(out name, out side1, out side2, out side3);
+
+                try
+                {
+                    double area = Triangle.GetArea(side1, side2, side3);
+                }
+                catch (UnableToBuildTriangleException ex)
+                {
+                    UI.PrintWarning(ex.Message);
+                    continue;
+                }
+
+                trSet.Add(new Triangle(name, side1, side2, side3));
+
+            } while (UI.GetUserConfirmation(PromptTypes.AddTriangle));
+
+            foreach (Triangle item in trSet)
+            {
+                Console.WriteLine(item);
+            }
+
+            return UI.GetUserConfirmation(PromptTypes.ExitApp);
+        }
+    }
+}
---
+++ 3_TriangleSort/3_TriangleSort/BL/TriangleComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_TriangleSort
+{
+    class TriangleComparer : IComparer<Triangle>
+    {
+
+        public int Compare(Triangle x, Triangle y)
+        {
+            return y.CompareTo(x);
+        }
+    }
+}
